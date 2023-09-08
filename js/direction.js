5c22b38268d39060eb9c3c9cb4c035a3235f63be
Janis <janis.paberzs18@gmail.com>
2023-09-08 18:51:46 +0300

    add slider in direction page

--- js/direction.js
+++ js/direction.js
@@ -1,8 +1,23 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  Fancybox.bind("[data-fancybox]", {
-    
-  });
+  Fancybox.bind("[data-fancybox]", {});
+
+  var swiperNews = new Swiper(".swiper-vacancy", {
+    slidesPerView: 1,
+    spaceBetween: 18,
+    loop: false,
+    breakpoints: {
+      // when window width is >= 320px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 35
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-buttons__next",
+      prevEl: ".swiper-buttons__prev"
+    },
+  }); // Слайдер ключевые проекты
 });
 //# sourceMappingURL=contacts.js.map
