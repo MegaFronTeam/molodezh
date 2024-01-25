109c83c85030d1d202ca68547eb3703009f64efc
jpaberzs <janis.paberzs18@gmail.com>
2024-01-26 00:44:48 +0200

    edit styles for slider on 100 page

--- js/aside-menu.js
+++ js/aside-menu.js
@@ -1,21 +1,22 @@
-"use strict";
+'use strict';
 
-document.addEventListener("DOMContentLoaded", function () {
-	let toggleMenu = document.querySelectorAll(".btn-reset--js");
-	for (const item of toggleMenu) {
-		item.addEventListener("click", function (e) {
-			item.nextElementSibling.classList.toggle("active");
-		})
-	}
+document.addEventListener('DOMContentLoaded', function () {
+  let toggleMenu = document.querySelectorAll('.btn-reset--js');
+  for (const item of toggleMenu) {
+    item.addEventListener('click', function (e) {
+      item.nextElementSibling.classList.toggle('active');
+    });
+  }
 
-	new Swiper(".gal-slider--js", {
+  new Swiper('.gal-slider--js', {
+    // slidesPerView: 4,
     slidesPerView: 'auto',
     spaceBetween: 32,
     speed: 300,
     loop: false,
-		pagination: {
-			el: ".slider .swiper-pagination",
-			type: "bullets"
-		}
-  })
-})
+    pagination: {
+      el: '.slider .swiper-pagination',
+      type: 'bullets',
+    },
+  });
+});
